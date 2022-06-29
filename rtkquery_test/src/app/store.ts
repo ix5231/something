ce6454649d3b060e62ad71362a00e19770a26236
ix5231 <ix5231if@gmail.com>
2022-06-29 20:18:17 +0900

    Update

---
+++ rtkquery_test/src/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+import { pokemonApi } from '../services/pokemon';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware)
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
