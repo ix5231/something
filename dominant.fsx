c718a39ffcd850ecd37a4782936aa3e25c77f598
Nave <ix5231if@gmail.com>
2022-07-02 08:32:21 +0900

    Update

--- dominant.fsx
+++ dominant.fsx
@@ -1,1 +1,8 @@
-let pitches = [| "C"; "C#"; "D"; "D#"; "E"; "F"; "G"; "G#"; "A"; "A#"; "B" |]
+let pitches = [| "C"; "C#"; "D"; "D#"; "E"; "F"; "F#"; "G"; "G#"; "A"; "A#"; "B" |]
+
+let tryDominantOf p =
+  match Array.tryFindIndex ((=) p) pitches with
+  | Some idx -> Array.tryItem ((idx + 7) % pitches.Length) pitches
+  | None -> None
+
+printfn "%A" (tryDominantOf (stdin.ReadLine()))
