e8fb20c215b55e2c8b270b611ebbbc3f5f58857d
nave <ix5231@gmail.com>
2022-07-02 17:18:21 +0900

    Update

--- jsoo-react-template/src/main.ml
+++ jsoo-react-template/src/main.ml
@@ -1,4 +1,10 @@
 open React.Dom.Dsl
 open Html
 
-let%component make () = div [||] [React.string "Hello world from OCaml"] 
+let%component make () =
+  let (count, set_count) = React.use_state (fun () -> 0) in
+  div [||] [
+    p [||] [React.string "Hello world from OCaml with Ix"];
+    p [||] [button [| onClick (fun _ -> set_count (fun c -> c + 1)) |] [React.string "Push!"]];
+    p [||] [React.string (Int.to_string count)]
+  ] 
